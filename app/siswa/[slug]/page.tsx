5ade9390a3611b1b22e013fd98c59646e737b126
Affan672 <affanabdullah1012@gmail.com>
2025-10-24 09:56:30 +0700

    foldernya di hapus

--- app/siswa/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-export default function Siswapage({ params }: { params: { slug: string } }) {
-  console.log("params slug", params);
-  return (
-    <>
-      <h1>halaman {params.slug} </h1>
-    </>
-  );
-}
